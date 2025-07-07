a70e06a15e67295589927dd174f41933e13d148d
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2025-07-07 23:54:19 +0300

    feat: update tailwind.config.ts to include additional content paths for heroui theme

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -4,7 +4,10 @@ import { heroui } from "@heroui/react";
 import typography from "@tailwindcss/typography";
 
 export default {
-  content: ["../../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "../../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
   darkMode: "class",
   plugins: [
     typography,
